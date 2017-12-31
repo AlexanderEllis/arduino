21c5ebbb80eeb914f3df7aabc134db1b8272dc45
Alexander Ellis <ellisonjtk@gmail.com>
2017-12-31 11:25:40 -0500

    Refactor server

--- simple-arduino-node/server.js
+++ simple-arduino-node/server.js
@@ -1,11 +1,15 @@
+/*
+  This is the very simple server code for listening to and responding to the arduino's button press.
+  It listens for data from the arduino, and if it receives an ON signal, it returns an ON signal. Likewise for OFF.
+*/
 var SerialPort = require('serialport');
+var PATH = '/dev/cu.usbmodemFD131';
 
-var port = new SerialPort('/dev/cu.usbmodemFD131', { baudRate: 9600 }, function(err) {
+var port = new SerialPort(PATH, { baudRate: 9600 }, function(err) {
   if (err) {
     console.log('Error: ', err.message);
   }
 });
-
 
 port.on('open', function() {
   port.on('data', function(data) {
